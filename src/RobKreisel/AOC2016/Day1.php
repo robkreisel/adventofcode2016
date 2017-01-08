945b1d1f372658c6a358c2696f3f9bc4ff6ff402
Rob Kreisel <nocom.gen@gmail.com>
2017-01-08 15:50:45 -0600

    Setting up first class and matching test

---
+++ src/RobKreisel/AOC2016/Day1.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace RobKreisel\AOC2016;
+
+
+class Day1
+{
+
+//R4, R3, L3, L2, L1, R1, L1, R2, R3, L5, L5, R4, L4, R2, R4, L3, R3, L3, R3, R4, R2, L1, R2, L3, L2, L1, R3, R5, L1, L4, R2, L4, R3, R1, R2, L5, R2, L189, R5, L5, R52, R3, L1, R4, R5, R1, R4, L1, L3, R2, L2, L3, R4, R3, L2, L5, R4, R5, L2, R2, L1, L3, R3, L4, R4, R5, L1, L1, R3, L5, L2, R76, R2, R2, L1, L3, R189, L3, L4, L1, L3, R5, R4, L1, R1, L1, L1, R2, L4, R2, L5, L5, L5, R2, L4, L5, R4, R4, R5, L5, R3, L1, L3, L1, L1, L3, L4, R5, L3, R5, R3, R3, L5, L5, R3, R4, L3, R3, R1, R3, R2, R2, L1, R1, L3, L3, L3, L1, R2, L1, R4, R4, L1, L1, R3, R3, R4, R1, L5, L2, R2, R3, R2, L3, R4, L5, R1, R4, R5, R4, L4, R1, L3, R1, R3, L2, L3, R1, L2, R3, L3, L1, L3, R4, L4, L5, R3, R5, R4, R1, L2, R3, R5, L5, L4, L1, L1
+
+// use x,y coordinate system
+// choose starting navigation point [0,0]
+// interpret navigation to update position
+// determine final position
+// determine distance from starting point to final position
+
+    public function calculateGridDistance($position1, $position2)
+    {
+        // Distance from (P1x,P1y) to (P2x,P2y) = | P1x - p2x | + | P1y -P2y |
+        return 5;
+    }
+
+}
---
+++ tests/RobKreisel/AOC2016/Tests/Day1Test.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace RobKreisel\AOC2016\Tests;
+
+
+use PHPUnit_Framework_TestCase;
+use RobKreisel\AOC2016\Day1;
+
+class Day1Test extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Day1
+     */
+    private $object;
+
+    public function setUp()
+    {
+        $this->object = new Day1();
+    }
+
+    public function testCalculateGridDistance()
+    {
+        $this->assertEquals(5, $this->object->calculateGridDistance([0,0],[-2,3]));
+    }
+}
