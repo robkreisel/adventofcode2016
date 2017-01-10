ecb6019c0759490f213e67e6c5a97cebae941daf
Rob Kreisel <nocom.gen@gmail.com>
2017-01-09 22:13:13 -0600

    Forgot loop to update position

--- day1.php
+++ day1.php
@@ -10,5 +10,9 @@ $navigation = file_get_contents(__DIR__ . '/instructions/day1.1-input.txt');
 
 $nav_array = explode(', ', $navigation);
 
+foreach ($nav_array as $n) {
+    $puzzle->updatePosition($n);
+}
+
 echo "Distance from start: " .
     $puzzle->calculateGridDistance($puzzle->getStartingPosition(), $puzzle->getCurrentPosition()) . "\n";
